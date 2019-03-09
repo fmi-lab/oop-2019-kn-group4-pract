f2343f6a53657709bc534bc12b1ec46c436acc6c
destroierdam <dam-bb@hotmail.com>
2019-03-09 22:31:30 +0200

    Add int * erase(int * arr, size_t size, size_t index) and void testErase(); correct space needed

--- Lesson3/solutions.cpp
+++ Lesson3/solutions.cpp
@@ -5,7 +5,9 @@ int * erase(int * arr, size_t size, size_t leftIndex, size_t rightIndex)
 	assert(rightIndex < size);
 	assert(rightIndex - leftIndex + 1 < size); // array will have at least 1 element
 
-	int * ans = new int[size - (rightIndex - leftIndex + 2)];
+	size_t spaceNeeded = size - (rightIndex - leftIndex + 1);
+
+	int * ans = new int[spaceNeeded];
 
 	for (size_t i = 0; i < leftIndex; i++)
 	{
@@ -18,4 +20,49 @@ int * erase(int * arr, size_t size, size_t leftIndex, size_t rightIndex)
 	}
 
 	return ans;
+}
+
+int * erase(int * arr, size_t size, size_t index)
+{
+	return erase(arr, size, index, index);
+}
+
+void testErase()
+{
+	size_t sizeOfArray = 10;
+
+	int * arr = new int[sizeOfArray]{ 1, 2, 3, 4, 5, 6, 7, 8 ,9, 10 };
+
+	// Test single index erase
+	int * result = erase(arr, sizeOfArray, 1);
+	assert(result[0] == 1);
+	assert(result[1] == 3);
+	assert(result[sizeOfArray-2] == 10);
+
+	// Print deleted array
+	cout << "Erasing elements at index 1: " << endl;
+	cout << "Array: ";
+	for (size_t i = 0; i < sizeOfArray - 1; i++)
+	{
+		cout << result[i] << " ";
+	}
+	cout << endl;
+
+	delete[] result;
+
+	// Test range erase
+	result = erase(arr, sizeOfArray, 0, 3);
+	assert(result[0] == 5);
+	assert(result[sizeOfArray - 5] == 10);
+
+	cout << "Erasing elements at indexes [0, 3]: "<<endl;
+	cout << "Array: ";
+	for (size_t i = 0; i < sizeOfArray - 4; i++)
+	{
+		cout << result[i] << " ";
+	}
+	cout << endl;
+
+	delete[] result;
+	delete[] arr;
 }
